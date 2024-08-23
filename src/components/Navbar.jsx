9d654f476407c6b7ed113de24b39cd7682beb72d
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-08-23 21:52:45 +0545

    logo updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
           window.scrollTo(0, 0);
         }}
       >
-        <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+        <img src={logo} alt='logo' className='w-9 h-9 object-contain rounded-r-full' />
         <p className='text-white text-[18px] font-bold cursor-pointer flex '>
           Priyanshu &nbsp;
           <span className='sm:block hidden'> | Mandal</span>
